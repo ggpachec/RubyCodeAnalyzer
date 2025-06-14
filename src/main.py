85fabd0003dd71b2120ec23885fd470eceaa4c97
luisluna2307 <laluna@espol.edu.ec>
2025-06-14 18:20:14 -0500

    Mejorar algoritmo caso de prueba #2 y generar log

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ tokens = (
     'MINUS',
     'TIMES',
     'DIVIDE',
-    #LUIS! EXPONENT
+    'EXPONENT',
 
 
     # Genesis Pacheco
@@ -69,7 +69,6 @@ tokens = (
     'LCORCH',
     'RCORCH',
     # Luis Luna - Inicio de aporte de nuevos tokens
-    'EXPONENT',
     'LBRACE',
     'RBRACE',
     'SEMICOLON',
@@ -193,8 +192,8 @@ lexer = lex.lex()
 
 # Joel Orrala - Inicio de bloque de generación de logs
 
-nombre_usuario = "joelorrala"  # cambiar por cada usuario Git
-archivo_prueba = "../src/algoritmos/algoritmo_joel.rb" # cambiar por el archivo de cada uno
+nombre_usuario = "luisluna2307"  # cambiar por cada usuario Git
+archivo_prueba = r"C:\Github\RubyCodeAnalyzer\src\algoritmos\algoritmo_luis.rb" # cambiar por el archivo de cada uno
 
 
 os.makedirs("logs", exist_ok=True) # Asegurar que la carpeta logs exista
