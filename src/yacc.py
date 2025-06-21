0027462f029cc8fa69408bbc666813c3081fcf87
luisluna2307 <laluna@espol.edu.ec>
2025-06-21 06:06:15 -0500

    Agregar reglas sintacticas para args, entrada, array, for, range

--- src/yacc.py
+++ src/yacc.py
@@ -1,13 +1,25 @@
 # Yacc example
-
+import datetime as dt
 import ply.yacc as yacc
 
 # Get the token map from the lexer.  This is required.
 from main import tokens
 
+
+# CAMBIAR Y RECORDAR que EQUALS y ASSIGN son diferentes en el contexto 
+# del lexer y del token que definimos
+#t_EQUALS = r'=='
+#t_ASSIGN = r'='
+
 def p_function(p):                                          #Genesis Pacheco
     '''function : DEF ID LPAREN RPAREN
     | DEF ID LPAREN body RPAREN'''
+    
+#Luis Luna
+def args(p):
+    '''args : ID
+            | ID COMMA args
+            | empty'''
 
 def p_body(p):
     '''body : sentence
@@ -19,11 +31,25 @@ def p_print(p):
 def p_sentence(p):
     '''sentence : assignment
         | expression
-        | print'''
+        | print
+        | input
+        | condition
+        | while_loop
+        | for_loop
+        | array
+        | range
+        | function
+        | class_def'''
 
-
+#CAMBIAR A ASSIGN
 def p_assignment (p):                                       #Genesis Pacheco
     'assignment : ID EQUALS factor'
+
+# Luis Luna - Inicio de la regla sintáctica para Ingreso de datos por teclado
+def p_input(p):
+    ''''input : PRINT STRING
+            | ID ASSIGN GETS DOT ID'''
+# Luis Luna - Fin de la regla sintáctica para Ingreso de datos por teclado
 
 
 def p_expression_plus(p):
@@ -41,10 +67,11 @@ def p_expression_term(p):
 def p_term_times(p):
     'term : term TIMES factor'
     #p[0] = p[1] * p[3]
-
+    
 def p_term_div(p):
     'term : term DIVIDE factor'
     #p[0] = p[1] / p[3]
+
 
 def p_term_factor(p):
     'term : factor'
@@ -62,9 +89,28 @@ def p_term_factor(p):
 def p_factor_valor(p):
     '''factor : INTEGER
                 | FLOAT
+                | STRING
+                | BOOLEAN
                 | ID'''
 
+# Luis Luna - Inicio de la regla sintáctica para estructura de datos array
+def p_array(p):
+    '''array : ID ASSIGN LCORCH elements RCORCH'''
 
+def p_elements(p):
+    '''elements : factor
+                | factor COMMA elements'''
+#Luis Luna - Fin de la regla sintáctica para estructura de datos array
+
+def p_for_loop(p):  #Luis Luna
+    'for_loop : FOR ID IN range body END'
+    
+def p_range(p): #Luis Luna
+    'range : LPAREN INTEGER RANGE_INCL INTEGER RPAREN'
+
+def p_empty(p):
+    'empty :'
+    pass
 
 def p_factor_expr(p):
     'factor : STRING'
@@ -72,14 +118,17 @@ def p_factor_expr(p):
 
 # Error rule for syntax errors
 def p_error(p):
-    print("Syntax error in input!")
+    if p:
+        print(f"Syntax error at token '{p.value}' (type {p.type}) at line {p.lineno}")
+    else:
+        print("Syntax error at EOF")
 
 # Build the parser
 parser = yacc.yacc()
 
 while True:
    try:
-       s = input('calc > ')
+       s = input('Ruby > ')
    except EOFError:
        break
    if not s: continue
